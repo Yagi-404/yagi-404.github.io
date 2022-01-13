d6ddee0b0b19b865ec79ec969025d95016975901
Léandro Laurent--Aquino <laurentaquino.leandro@gmail.com>
2022-01-13 19:25:28 +0100

    working buttons

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 // http://127.0.0.1:5500/
+
+const githubBtn = document.querySelector('#github');
+const discordBtn = document.querySelector('#discord');
+
+githubBtn.addEventListener('click', () => {
+    window.location.replace('https://github.com/Yagi-404');
+});
+
+discordBtn.addEventListener('click', () => {
+    window.location.replace('https://discord.gg/SRbUfZ2v6m');
+})
